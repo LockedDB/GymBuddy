c86f3b934adf1bb5a4515afa3eaa9537feaa4a43
Daniel <daniel.benet@outlook.es>
2023-10-13 17:11:47 +0200

    Initial Commit

---
+++ GymBuddy/GymBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymBuddyApp.swift
+//  GymBuddy
+//
+//  Created by Dani Benet on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GymBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
